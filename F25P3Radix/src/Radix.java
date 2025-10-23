81d1f70f995322ea34eecf4d57972e284cb0cd2a
edwinb1067 <edwinb1067@vt.edu>
2025-10-23 15:00:05 -0400

    Added the radix sort from OpenDSA

--- F25P3Radix/src/Radix.java
+++ F25P3Radix/src/Radix.java
@@ -29,5 +29,37 @@ public class Radix {
      * @throws IOException
      */
     private void radixSort() throws IOException {
+    	Integer[] array = new Integer[112500];
+    	radix(array, 32, 8);
     }
+    
+    /**
+     * 
+     * @param A Array that we are sorting.
+     * @param k	total bits per data in A
+     * @param r bits per digit sorted by (sort 2 ints at a time for 8 bits)
+     */
+    static void radix(Integer[] A, int k, int r) {
+    	  Integer[] B = new Integer[A.length];
+    	  int[] count = new int[r];     // Count[i] stores number of records with digit value i
+    	  int i, j, rtok;
+
+    	  for (i=0, rtok=1; i<k; i++, rtok*=r) { // For k digits
+    	    for (j=0; j<r; j++) { count[j] = 0; }    // Initialize count
+
+    	    // Count the number of records for each bin on this pass
+    	    for (j=0; j<A.length; j++) { count[(A[j]/rtok)%r]++; }
+
+    	    // After processing, count[j] will be index in B for first slot of bin j.
+    	    int total = A.length;
+    	    for (j=r-1; j>=0; j--) { total -= count[j]; count[j] = total; }
+
+    	    // Put records into bins, working from left to right
+    	    for (j=0; j<A.length; j++) {
+    	      B[count[(A[j]/rtok)%r]] = A[j];
+    	      count[(A[j]/rtok)%r] = count[(A[j]/rtok)%r] + 1;
+    	    }
+    	    for (j=0; j<A.length; j++) { A[j] = B[j]; } // Copy B back
+    	  }
+    	}
 }
